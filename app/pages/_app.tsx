9d0a91487a5f26aa9aaf10be5aab25b778d6de5e
NozomuTsuruta <tuno.0806@icloud.com>
2020-12-12 17:26:10 +0900

    フロントとサーバー統合

---
+++ app/pages/_app.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react';
+import { AppProps } from 'next/app';
+import { ThemeProvider as StyledComponentsThemeProvider } from 'styled-components';
+import { theme } from '../utils';
+//material
+import { ThemeProvider as MaterialUIThemeProvider } from '@material-ui/core/styles';
+import { StylesProvider } from '@material-ui/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+/**
+ * クライアント側のレンダリングカスタマイズ
+ * 全てのpageをラップ
+ */
+export default function App({ Component, pageProps }: AppProps) {
+  /**
+   * サーバー側に挿入されたCSSを削除
+   */
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }, []);
+
+  return (
+    <StylesProvider injectFirst>
+      <MaterialUIThemeProvider theme={theme}>
+        <StyledComponentsThemeProvider theme={theme}>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </StyledComponentsThemeProvider>
+      </MaterialUIThemeProvider>
+    </StylesProvider>
+  );
+}
