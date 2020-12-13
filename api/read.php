4883d9964e8f294cb607d0ed4b705ff86fd3f697
NozomuTsuruta <tuno.0806@icloud.com>
2020-12-13 16:13:11 +0900

    読み込み機能完成

--- api/read.php
+++ api/read.php
@@ -1,1 +1,11 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET');
+
+require_once 'db_connection.php';
+
+$data = $db->query('SELECT * FROM todos')->fetchAll(PDO::FETCH_ASSOC);
+
+echo json_encode([
+    'todos'=>$data,
+]);
